5de549a06d5b7b1f710616f4f5b69322519eec5d
Amandeep <singh.amandeep5665@gmail.com>
2022-04-11 13:20:59 -0700

    working model

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-const apiKey = "m730i1pNJ7wdGAC6u7pSD0hzt8WbJ_Jv2EmMmf82-uc";
-const count = 30;
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
+let photosArray = [];
+
+const apiKey = "m730i1pNJ7wdGAC6u7pSD0hzt8WbJ_Jv2EmMmf82-uc";
+const count = 30;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 //https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&orientation=landscape
-const imageContainer = document.getElementById("image-container");
-
-const loader = document.getElementById("loader");
-
-let photosArray = [];
 
 // check if all images are loaded
 function imageLoaded() {
   imagesLoaded++;
-  console.log(imagesLoaded);
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
-    console.log("ready = ", ready);
   }
 }
 
@@ -34,8 +31,8 @@ function setAttributes(element, attributes) {
 // Create Elements For Links and Photos, add to DOM
 function displayPhotos() {
   imagesLoaded = 0;
-  displayPhotos = photosArray.length;
-  console.log("Total images", totalImages);
+  totalImages = photosArray.length;
+
   photosArray.forEach((photo) => {
     //Create <a> to link to Unsplash
     const item = document.createElement("a");
@@ -51,9 +48,8 @@ function displayPhotos() {
     img.setAttribute("src", photo.urls.regular);
     img.setAttribute("alt", photo.alt_description);
     img.setAttribute("title", photo.alt_description);
-    img.addEventListener("load", imageLoaded());
+    img.addEventListener('load', imageLoaded);
 
-    
     //put img inside anchor element, put both inside container
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -65,7 +61,6 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
     displayPhotos();
   } catch (error) {
     // catch error here
@@ -76,7 +71,7 @@ async function getPhotos() {
 window.addEventListener("scroll", () => {
   if (
     window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
-    ready == true
+    ready
   ) {
     ready = false;
     getPhotos();
